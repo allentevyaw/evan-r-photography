2fa6fe26456d4b0eee30bb2b338103d8b13e0fb5
allentevyaw <atevyaw@gmail.com>
2023-02-05 00:22:11 -0500

    projects added

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {AiOutlineCamera, AiOutlineVideoCamera, AiFillCamera} from 'react-icons/ai'
-import Shoreline from '../assets/shoreline.png'
+import Rocks from '../assets/rocks.jpg'
 
 const About = () => {
   return (
@@ -8,12 +8,12 @@ const About = () => {
 
 
 <div className="container mx-auto text-center lg:text-left pt-7 pb-7">
-      <div className="grid lg:grid-cols-2 gap-4 items-center">
-        <div className="mb-12 lg:mb-0 ">
+      <div className="grid lg:grid-cols-2 gap-2 items-center">
+        <div className="mb-12 lg:mb-0 px-4">
           <div className="block rounded-lg shadow-lg px-6 py-12 md:px-12 lg:-mr-16"
             style={{background: "hsla(0, 0%, 100%, 0.3)", backdropfilter: "blur(30px)"}}>
             <h2 className="text-3xl font-bold mb-6 text-black">About Me</h2>
-            <p className="text-gray-500 mb-6 pb-2 lg:pb-0">
+            <p className="text-gray-600 mb-6 pb-2 lg:pb-0">
             With years of experience, I am a master of my craft, blending creativity and technical expertise to create stunning works of art. My portfolio speaks for itself, showcasing my versatility and ability to capture the essence of a moment.
             </p>
 
@@ -35,16 +35,16 @@ const About = () => {
             </div>
 
             <p className="text-gray-500 mb-0">
-            <p className='py-4'>As a wedding photographer, I understand the importance 
+            <p className='py-4'>As a <span className='text-[#027E72] underline'>wedding photographer</span>, I understand the importance 
             of preserving memories and emotions of a couple's special day. I am dedicated 
             to capturing every candid moment, and creating posed portraits that are timeless. 
-            For headshots, I bring out the best in my clients, whether they are actors, models or 
+            For <span className='text-[#027E72] underline'> headshots</span>, I bring out the best in my clients, whether they are actors, models or 
             professionals. My goal is to create a headshot that stands out and helps them achieve 
-            their goals. Erotic photography is my passion. I believe that every body is unique and 
+            their goals. <span className='text-[#027E72] underline'>Erotic photography</span> is my passion. I believe that every body is unique and 
             beautiful, and I know how to capture the sensuality and eroticism of the human form. Whether 
             you want to capture a special moment or create art that celebrates the beauty of the human 
             form, I am the perfect choice. In addition to my photography expertise, I am also a skilled 
-            videographer and drone photographer. I bring the same level of creativity and technical 
+            <span className='text-[#027E72] underline'> videographer and drone photographer.</span> I bring the same level of creativity and technical 
             expertise to my videography and drone work, ensuring that every project is captured in 
             stunning detail.</p>
             </p>
@@ -53,8 +53,8 @@ const About = () => {
 
         <div className='px-4'>
           <img 
-          src={Shoreline} 
-          className="rounded-lg shadow-lg shadow-gray-400 hover:blur-sm"
+          src={Rocks} 
+          className="rounded-lg shadow-lg shadow-gray-400 hover:opacity-70"
           alt="" />
         </div>
       </div>
--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import Shoreline from '../assets/shoreline.png'
 
 const Projects = () => {
   return (
-    <div name='projects' className='w-full min-h-screen flex justify-center'>
-        <h1 className='text-amber-200 text-4xl mt-24 underline'>Projects</h1>
+    <div name='projects' className='w-full min-h-screen bg-[#027E72] py-10'>
+        <h1 className='text-amber-200 text-4xl mt-24 underline flex justify-center pb-10'>Projects</h1>
+
+<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 px-4">
+
+
+    <div className='relative group container shadow-lg rounded-lg overflow-hidden'>
+      <div className='flex justify-start'>
+      <img src={Shoreline}
+          className="w-full align-middle h-full hover:scale-125 duration-500 transition-all ease-in-out" />
+      <h1 className='absolute text-2xl text-white p-2'>Canada</h1>
+      </div>
+    </div>
+
+
+    <div className='relative group container shadow-lg rounded-lg overflow-hidden'>
+      <div className='flex justify-start'>
+      <img src={Shoreline}
+          className="w-full align-middle h-full hover:scale-125 duration-500 transition-all ease-in-out" />
+      <h1 className='absolute text-2xl text-white p-2'>Canada</h1>
+      </div>
+    </div>
+
+
+    <div className='relative group container shadow-lg rounded-lg overflow-hidden'>
+      <div className='flex justify-start'>
+      <img src={Shoreline}
+          className="w-full align-middle h-full hover:scale-125 duration-500 transition-all ease-in-out" />
+      <h1 className='absolute text-2xl text-white p-2'>Canada</h1>
+      </div>
+    </div>
+
+    </div>
     </div>
   )
 }
